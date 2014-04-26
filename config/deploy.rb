eb26f3adcc16a88f1ba653dc23f9364195b2e677
Damiano Renfer <damiano.renfer@he-arc.ch>
2014-04-26 15:36:55 +0200

    Modified : Environnement variables names for secret keys Modified : linked files for deploy, added database.yml and secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,13 @@ set :deploy_to, '/var/www/npm'
 # set :pty, true
 
 # Default value for :linked_files is []
-#set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+#set :default_env, { }
 
 # Default value for keep_releases is 5
 set :keep_releases, 2
